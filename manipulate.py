0523b7037ac45d0c6dcc743309c137209a1e6fe9
Nick Robinson <nick@isengard.io>
2016-07-09 10:53:13 -0400

    Problem solved

--- manipulate.py
+++ manipulate.py
@@ -1,11 +1,14 @@
 import numpy as np
 
 transitions = np.array([[[0,1,0], [0, 0, 1]], [[0, 1, 0], [0, 1, 0]], [[0, 0, 1], [0, 0, 1]]])
+#
+# morphed = np.array([], dtype=np.int64).reshape(0, transitions.shape[0])
+#
+# for i in xrange(transitions.shape[0]):
+#     print transitions[i][0,:]
+#     morphed = np.vstack((morphed, transitions[i][0]))
+#
+# print morphed
 
-morphed = np.array([], dtype=np.int64).reshape(0, transitions.shape[0])
-
-for i in xrange(transitions.shape[0]):
-    print transitions[i][0,:]
-    morphed = np.vstack((morphed, transitions[i][0]))
-
-print morphed
+#a = np.array([[[1, 2],[3, 4]], [[5, 6], [7, 8]]])
+print np.swapaxes(transitions, 0, 1)
